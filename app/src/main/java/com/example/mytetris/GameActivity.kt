0f8c37e4d6baeef58d70b345d271b0d096a3e118
picalfer <trable228@icloud.com>
2022-04-10 22:52:36 +0300

    xml for testing logic is done

--- app/src/main/java/com/example/mytetris/GameActivity.kt
+++ app/src/main/java/com/example/mytetris/GameActivity.kt
@@ -2,10 +2,30 @@ package com.example.mytetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytetris.databinding.ActivityGameBinding
+import com.example.mytetris.storage.AppPreferences
 
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var b: ActivityGameBinding
+    var appPreferences: AppPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        b = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        appPreferences = AppPreferences(this)
+
+        updateHighScore()
+        updateCurrentScore()
+    }
+
+    private fun updateHighScore() {
+        b.tvHighScore.text = "${appPreferences?.getHighScore()}"
+    }
+
+    private fun updateCurrentScore() {
+        b.tvCurrentScore.text = "0"
     }
 }
