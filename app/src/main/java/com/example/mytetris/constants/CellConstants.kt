75f2f4a611d59452115421a4f9a4e09b07662e57
picalfer <trable228@icloud.com>
2022-05-06 13:13:39 +0300

    AppModel.kt on work to, part of functions are done

---
+++ app/src/main/java/com/example/mytetris/constants/CellConstants.kt
@@ -0,0 +1,5 @@
+package com.example.mytetris.constants
+
+enum class CellConstants(val value: Byte) {
+    EMPTY(0), EPHEMERAL(1)
+}
--- app/src/main/java/com/example/mytetris/models/AppModel.kt
+++ app/src/main/java/com/example/mytetris/models/AppModel.kt
@@ -1,11 +1,13 @@
 package com.example.mytetris.models
 
+import android.graphics.Point
+import com.example.mytetris.constants.CellConstants
 import com.example.mytetris.constants.FieldConstants
 import com.example.mytetris.helpers.array2dOfByte
 import com.example.mytetris.storage.AppPreferences
 
 class AppModel {
-    var scope: Int = 0
+    var score: Int = 0
     private var preferences: AppPreferences? = null
 
     var currentBlock: Block? = null
@@ -15,6 +17,125 @@ class AppModel {
         FieldConstants.ROW_COUNT.value,
         FieldConstants.COLUMN_COUNT.value
     )
+
+    fun setPreferences(preferences: AppPreferences?) { // устанавливаем в качестве аргумента
+        // присланный SharedPreferences файл
+        this.preferences = preferences
+    }
+
+    fun getCellStatus(row: Int, column: Int) : Byte? {
+        return field[row][column]
+    }
+
+    private fun setCellStatus(row: Int, column: Int, status: Byte?) {
+        if (status != null) {
+            field[row][column] = status
+        }
+    }
+
+    fun isGameActive() : Boolean {
+        return currentState == Statuses.ACTIVE.name
+    }
+
+    fun isGameAwaitingStart() : Boolean {
+        return currentState == Statuses.AWAITING_START.name
+    }
+
+    fun isGameOver() : Boolean {
+        return currentState == Statuses.OVER.name
+    }
+
+    private fun boostScore() {
+        score += 10
+        if (score > preferences?.getHighScore() as Int) preferences?.saveHighScore(score)
+    }
+
+    private fun generateNextBlock() {
+        currentBlock = Block.createBlock()
+    }
+
+    private fun validTranslation(position: Point, shape: Array<ByteArray>): Boolean {
+        // 1-ые 3 условия проверяют, находится ли в поле позиция, куда передвигаем блок
+        // в else проверяется свободны ли клетки куда передвигаем блок
+        return if (position.y < 0 || position.x < 0) {
+            false
+        } else if (position.y + shape.size > FieldConstants.ROW_COUNT.value) {
+            false
+        } else if (position.x + shape[0].size > FieldConstants.COLUMN_COUNT.value) {
+            false
+        } else {
+            for (i in 0 until shape.size) {
+                for (j in 0 until shape[i].size) {
+                    val y = position.y + i
+                    val x = position.x + j
+                    if (CellConstants.EMPTY.value != shape[i][j] &&
+                        CellConstants.EMPTY.value != field[x][y]
+                    ) {
+                        return false
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    private fun moveValid(position: Point, frameNumber: Int?) : Boolean {
+        // функция для запуска функции validTranslation
+        val shape: Array<ByteArray>? = currentBlock?.getShape(frameNumber as Int)
+        return validTranslation(position, shape as Array<ByteArray>)
+    }
+
+    fun generateField(action: String) {
+        if (isGameActive()) {
+            resetField()
+            var frameNumber: Int? = currentBlock?.frameNumber
+            val coordinate: Point? = Point()
+            coordinate?.x = currentBlock?.position?.x
+            coordinate?.y = currentBlock?.position?.y
+
+            when (action) {
+                Motions.LEFT.name -> {
+                    coordinate?.x = currentBlock?.position?.x?.minus(1)
+                }
+                Motions.RIGHT.name -> {
+                    coordinate?.x = currentBlock?.position?.x?.plus(1)
+                }
+                Motions.DOWN.name -> {
+                    coordinate?.y = currentBlock?.position?.y?.plus(1)
+                }
+                Motions.ROTATE.name -> {
+                    frameNumber = frameNumber?.plus(1)
+                    if (frameNumber != null) {
+                        if (frameNumber >= currentBlock?.frameCount as Int) {
+                            frameNumber = 0
+                            // если frame number станет больше чем возможное количество
+                            // форм, то оно вернется в исходное состояние
+                        }
+                    }
+                }
+            }
+
+            if (!moveValid(coordinate as Point, frameNumber)) {
+                translateBlock(currentBlock?.position as Point, currentBlock?.frameNumber as Int)
+                if (Motions.DOWN.name == action) {
+                    boostScore()
+                    persistCellData()
+                    assessField()
+                    generateNextBlock()
+                    if (!blockAdditionPossible()) {
+                        currentState = Statuses.OVER.name
+                        currentBlock = null
+                        resetField(false)
+                    }
+                }
+            } else {
+                if (frameNumber != null) {
+                    translateBlock(coordinate, frameNumber)
+                    currentBlock?.setState(frameNumber, coordinate)
+                }
+            }
+        }
+    }
 
     enum class Statuses {
         AWAITING_START, // состояние игры до ее запуска
